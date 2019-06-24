fc6524379bbce43954d1897343d9c6003936da2c
shfksthgml@naver.com <LeeSohee>
2019-06-24 09:19:55 +0900

    <AnimationPractice.c> 애니메이션 연습

---
+++ app/src/main/java/com/example/animationpractice/Fragment/MainFragment.kt
@@ -0,0 +1,33 @@
+package com.example.animationpractice.Fragment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+import com.example.animationpractice.R
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+    private lateinit var root : ViewGroup
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        root = inflater.inflate(R.layout.fragment_main, container, false) as ViewGroup
+        return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        button_animatelayoutchanges.setOnClickListener {
+            findNavController().navigate(R.id.)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/animationpractice/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.animationpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        Log.e("TAG","onBack")
+    }
+}
